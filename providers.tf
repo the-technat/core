cd3477bc1c9e2113ec75963b73e9f4251f15d11a
Nathanael Liechti <technat@technat.ch>
2024-09-30 12:12:16 +0200

    fix: use proper variables for tailscale provider

--- providers.tf
+++ providers.tf
@@ -1,4 +1,5 @@
 provider "tailscale" {
-  api_key = var.tailscale_api_key
-  tailnet = var.tailscale_tailnet
+  tailnet             = var.tailscale_tailnet
+  oauth_client_id     = var.tailscale_oauth_client_id
+  oauth_client_secret = var.tailscale_oauth_client_secret
 }
