c809ce67005143431e58e6d3a5302b1b390fafa0
Nathanael Liechti <technat@technat.ch>
2024-10-03 13:07:10 +0200

    feat: remove app installation file

--- app_installations.tf
+++
@@ -1,5 +0,0 @@
-resource "github_app_installation_repositories" "the-technat-renovate" {
-  # The installation id of the app (in the organization).
-  installation_id       = "1014986" # self-hosted renovate app
-  selected_repositories = data.github_repositories.all_repos.full_names
-}
