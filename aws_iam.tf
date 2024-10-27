9e7888ffd6364aff85629265e1665a4f29ac9f63
Nathanael Liechti <technat@technat.ch>
2024-10-27 18:08:38 +0000

    feat: add tag to oidc provider

--- aws_iam.tf
+++ aws_iam.tf
@@ -6,6 +6,10 @@ resource "aws_iam_openid_connect_provider" "github_actions" {
   ]
 
   thumbprint_list = ["1b511abead59c6ce207077c0bf0e0043b1382612"]
+ 
+  tags = {
+    managed-by = "terraform"
+  }
 }
 
 data "aws_iam_policy_document" "nuker_trust" {
