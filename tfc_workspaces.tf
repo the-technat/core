d1ce5d2ecf9fe9d12b1a565f94ca28b7f3900450
Nathanael Liechti <technat@technat.ch>
2024-10-27 16:55:09 +0000

    chore: pin tf version and dir for cappy workspace

--- tfc_workspaces.tf
+++ tfc_workspaces.tf
@@ -1,10 +1,12 @@
 resource "tfe_workspace" "cappy" {
-  name         = "cappy"
-  organization = data.tfe_organization.technat.name
-  project_id   = tfe_project.cappy.id
-  auto_apply   = true
+  name              = "cappy"
+  organization      = data.tfe_organization.technat.name
+  project_id        = tfe_project.cappy.id
+  auto_apply        = true
+  working_directory = "mgmt_cluster"
+  terraform_version = "1.9.8"
   vcs_repo {
-    identifier = "the-technat/cappy"
+    identifier                 = "the-technat/cappy"
     github_app_installation_id = local.tfc_github_app_installation_id
   }
 }
