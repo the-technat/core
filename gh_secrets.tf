37c56e3bc9d57bf94d5e4cb5a2f0abba5e84bcaf
Nathanael Liechti <technat@technat.ch>
2024-10-29 20:19:09 +0000

    feat: add action secrets for account nuker

--- gh_secrets.tf
+++ gh_secrets.tf
@@ -3,3 +3,21 @@ resource "github_actions_secret" "account-nuker-aws-role" {
   secret_name     = "aws_role"
   plaintext_value = aws_iam_role.nuker.arn
 }
+resource "github_actions_secret" "account-nuker-azure-client-id" {
+  repository      = github_repository.account-nuker.name
+  secret_name     = "AZURE_CLIENT_ID"
+  plaintext_value = azuread_service_principal.nuker.client_id
+}
+
+resource "github_actions_secret" "account-nuker-azure-tenant-id" {
+  repository      = github_repository.account-nuker.name
+  secret_name     = "AZURE_TENANT_ID"
+  plaintext_value = data.azurerm_subscription.current.tenant_id
+}
+
+resource "github_actions_secret" "account-nuker-azure-subscription-id" {
+  repository      = github_repository.account-nuker.name
+  secret_name     = "AZURE_SUBSCRIPTION_ID"
+  plaintext_value = data.azurerm_subscription.current.subscription_id
+}
+
