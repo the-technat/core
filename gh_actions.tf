4a4d8c36b608ac914fd1a9b28d53085e35a41aa4
Nathanael Liechti <technat@technat.ch>
2024-10-31 19:07:03 +0000

    feat: switch from secrets to variables

---
+++ gh_actions.tf
@@ -0,0 +1,22 @@
+resource "github_actions_secret" "account-nuker-aws-role" {
+  repository      = github_repository.account-nuker.name
+  secret_name     = "aws_role"
+  plaintext_value = aws_iam_role.nuker.arn
+}
+
+resource "github_actions_variable" "account-nuker-azure-client-id" {
+  repository      = github_repository.account-nuker.name
+  variable_name     = "AZURE_CLIENT_ID"
+  value = azuread_service_principal.nuker.client_id
+}
+resource "github_actions_variable" "account-nuker-azure-tenant-id" {
+  repository      = github_repository.account-nuker.name
+  variable_name    = "AZURE_TENANT_ID"
+  value = data.azurerm_subscription.current.tenant_id
+}
+resource "github_actions_variable" "account-nuker-azure-subscription-id" {
+  repository      = github_repository.account-nuker.name
+  variable_name     = "AZURE_SUBSCRIPTION_ID"
+  value = data.azurerm_subscription.current.subscription_id
+}
+
