0e596fbb146446c7d7950ea099a904e06ec8449f
Nathanael Liechti <technat@technat.ch>
2024-11-03 14:03:12 +0000

    feat: remove grapes hcloud token

--- gh_actions.tf
+++ gh_actions.tf
@@ -24,15 +24,6 @@ resource "github_actions_secret" "account-nuker-azure-subscription-id" {
   secret_name     = "AZURE_SUBSCRIPTION_ID"
   plaintext_value = data.azurerm_subscription.current.subscription_id
 }
-data "hcp_vault_secrets_secret" "grapes_hcloud_token" {
-  app_name    = hcp_vault_secrets_app.core.app_name
-  secret_name = "GRAPES_HCLOUD_TOKEN"
-}
-resource "github_actions_secret" "account-nuker-hcloud-token-grapes" {
-  repository      = github_repository.account-nuker.name
-  secret_name     = "GRAPES_HCLOUD_TOKEN"
-  plaintext_value = data.hcp_vault_secrets_secret.grapes_hcloud_token.secret_value
-}
 data "hcp_vault_secrets_secret" "tevbox_hcloud_token" {
   app_name    = hcp_vault_secrets_app.core.app_name
   secret_name = "TEVBOX_HCLOUD_TOKEN"
