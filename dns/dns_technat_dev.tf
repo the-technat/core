6d7e94f111ada2630a8443daf92e089eb2a58513
Nathanael Liechti <technat@technat.ch>
2024-12-07 17:34:23 +0100

    feat: add ns for custom sslip.io domain

--- dns/dns_technat_dev.tf
+++ dns/dns_technat_dev.tf
@@ -5,3 +5,24 @@ resource "hetznerdns_zone" "technat_dev" {
 }
 
 # Note: these are not all records in the zone, but the static ones
+resource "hetznerdns_record" "ns_local_technat_dev_1" {
+  zone_id = hetznerdns_zone.technat_dev.id
+  name    = "local"
+  value   = "ns-gce.sslip.io."
+  type    = "NS"
+  ttl     = 300
+}
+resource "hetznerdns_record" "ns_local_technat_dev_2" {
+  zone_id = hetznerdns_zone.technat_dev.id
+  name    = "local"
+  value   = "ns-hetzner.sslip.io."
+  type    = "NS"
+  ttl     = 300
+}
+resource "hetznerdns_record" "ns_local_technat_dev_3" {
+  zone_id = hetznerdns_zone.technat_dev.id
+  name    = "local"
+  value   = "ns-ovh.sslip.io."
+  type    = "NS"
+  ttl     = 300
+}
